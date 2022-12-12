ccae7d98f0f04ec79f7ce302e902aed7e2ad52cd
kanhaiya1007 <92618657+kanhaiya1007@users.noreply.github.com>
2022-12-12 05:56:21 -0800

    add

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(()=>{
+    $(window).scroll(()=>{
+        if(this.scrollY>20){
+            $("#navbar").addClass("sticky");
+        }else{
+            $("#navbar").removeClass("sticky");
+        }
+    })
+})
